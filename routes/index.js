c9937044a18b8aa9231cda8037ff5b43124d4451
Igor Simoes da Silvera <igor01silveira@gmail.com>
2024-09-08 22:38:27 -0300

    Criando as Páginas de Livro individual, Contato e Carrinho de compras

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,7 @@ router.get('/contact', frontController.renderContactPage);
 router.get('/user', frontController.renderUserPage);
 router.get('/login', frontController.renderLoginPage);
 router.post('/login', frontController.renderAuthLoginPage);
+router.get('/books/:id', frontController.renderBookPage);
+router.get('/cart', frontController.renderCartPage);
 
 module.exports = router;
